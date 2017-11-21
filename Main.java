52b0bcaffa2e085b55dbdeef2b37586005a04477
Gina Gonzalez-Roundey <ginagonzalezroundey@gmail.com>
2017-11-21 12:01:20 -0500

    Final

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ public class Main {
     */
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                    System.out.println("FizzBuzz");
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
